f4cde2a22b8f97e28e40aa011429424555f9431b
Ciin Noguera <130929079+CiinNoguera@users.noreply.github.com>
2025-07-31 11:39:30 -0300

    Creación de nuevos modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,19 @@ import { ConfigModule } from '@nestjs/config';
 import { InterludesModule } from './interludes/interludes.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { OptionsModule } from './options/options.module';
+import { HistoryModule } from './history/history.module';
+import { UserModule } from './users/user.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     MongooseModule.forRoot(process.env.MONGO_URI),
     InterludesModule,
-    OptionsModule],
+    OptionsModule,
+    HistoryModule,
+    UserModule,
+    AuthModule],
   controllers: [],
   providers: [],
 })
